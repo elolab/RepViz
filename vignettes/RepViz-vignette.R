d533ce89150620985baba94da1c00dec7cfb6b92
Thomas <faux.thomas1@gmail.com>
2019-03-14 14:32:32 +0200

    Upgrade to the version 0.1.16, and update the vignette

--- vignettes/RepViz-vignette.R
+++ vignettes/RepViz-vignette.R
@@ -8,7 +8,7 @@ opts_knit$set(root.dir = system.file("extdata", package = "RepViz"))
 
 ## ----input BAM, echo=FALSE-----------------------------------------------
 BAM_table <- read.table(system.file("extdata","BAM_input.csv", package = "RepViz"), sep = ",")
-colnames(BAM_table) <- c("bam file","group","group name")
+colnames(BAM_table) <- c("bam file","group")
 knitr::kable(BAM_table)
 
 ## ----input BED, echo=FALSE-----------------------------------------------
@@ -29,7 +29,8 @@ RepViz::RepViz(region = region,
                BAM = "BAM_input.csv",
                BED = "BED_input.csv",
                avgTrack = T,
-               geneTrack = T)
+               geneTrack = T,
+               verbose = F)
 
 
 
